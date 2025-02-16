99dac2eb5ec670041f8f844fc26ce518e31e41b7
Danmark12 <danmarkpetalcurin@gmail.com>
2025-02-16 20:27:43 +0800

    added Details in Menu

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -17,11 +17,13 @@ import {
   import { Redirect, Route } from 'react-router';
   import Home from './Home';
   import About from './About';
+  import Details from './Details';
   
     const Menu: React.FC = () => {
       const path = [
           {name:'Home', url: '/it35-lab/app/home', icon: homeOutline},
           {name:'About', url: '/it35-lab/app/about', icon: rocketOutline},
+          
       ]
   
       return (
@@ -53,6 +55,7 @@ import {
                       <IonRouterOutlet id="main">
                           <Route exact path="/it35-lab/app/home" component={Home} />
                           <Route exact path="/it35-lab/app/about" component={About} />
+                          <Route exact path="/it35-lab/app/home/details" component={Details} />
       
                           <Route exact path="/it35-lab/app">
                               <Redirect to="/it35-lab/app/home"/>
