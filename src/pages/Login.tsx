e3d47483a05d3a2eb4b3aac0da05be7c75248472
Danmark12 <danmarkpetalcurin@gmail.com>
2025-02-27 14:19:31 +0800

    added ioninput for passwoord and name for login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,8 +8,15 @@ import {
     IonPage, 
     IonTitle, 
     IonToolbar, 
-    useIonRouter
+    useIonRouter,
+    IonAlert,
+    IonAvatar,
+    IonIcon, 
+    IonToast   
 } from '@ionic/react';
+import React from 'react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import { IonInput, IonInputPasswordToggle } from '@ionic/react'
 
 const Login: React.FC = () => {
   const navigation = useIonRouter();
@@ -24,7 +31,16 @@ const Login: React.FC = () => {
           <IonTitle>Login</IonTitle>
         </IonToolbar>
       </IonHeader>
+      
+
       <IonContent className='ion-padding'>
+
+        <IonInput label="Name" placeholder="Enter company name"></IonInput>
+
+      <IonInput type="password" label="Password" value="NeverGonnaGiveYouUp">
+      <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+    </IonInput>
+        
           <IonButton onClick={() => doLogin()} expand="full">
               Login
           </IonButton>
