6ec235fdd5f4a14bf3c05f900216879d0163a1ec
Danmark12 <danmarkpetalcurin@gmail.com>
2025-03-17 10:55:17 +0800

    Updates

--- assets/md.transition-legacy-Ca42I5nY.js
+++ assets/md.transition-legacy-Ca42I5nY.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B04VHBAR.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-DpZGtL7i.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
