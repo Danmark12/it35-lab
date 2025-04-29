8ab76006ba276cecbe899d079dc35b3e39fe990e
Danmark12 <danmarkpetalcurin@gamil.com>
2025-04-29 16:58:46 +0800

    Updates

--- assets/ios.transition-legacy-Dev0-Pwc.js
+++ assets/ios.transition-legacy-Dev0-Pwc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C7QFpBBK.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BAPeWKYx.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-BTdPUFwn.js
+++ assets/swipe-back-legacy-BTdPUFwn.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C7QFpBBK.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BAPeWKYx.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
